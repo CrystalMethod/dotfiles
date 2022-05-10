8b5d11def907778ce0b14962fd695f67287be82c
Lars Martin <lars.martin@smb-tec.com>
2022-05-10 22:00:38 +0200

    add plugin vista and initial configuration

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -87,6 +87,7 @@ return packer.startup(function(use)
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   use "tami5/lspsaga.nvim"
+  use "liuchengxu/vista.vim"
 
   use "j-hui/fidget.nvim" -- show lsp progress
 
