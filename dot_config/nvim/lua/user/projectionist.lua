3ea4fd7ad1c231cd7d0fa57cbc757acbd5f697ff
Lars Martin <lars.martin.ext@zeiss.com>
2022-05-21 10:48:33 +0200

    add alternative file configuration for Java programming language

--- dot_config/nvim/lua/user/projectionist.lua
+++ dot_config/nvim/lua/user/projectionist.lua
@@ -2,10 +2,22 @@ vim.g.projectionist_heuristics = {
   ["build.gradle|pom.xml"] = {
     ["src/main/java/*.java"] = {
       type = "source",
+      template = {
+        "package {dirname|dot};",
+        "",
+        "public class {basename} {open}",
+        "{close}",
+      },
       alternate = "src/test/java/{}Test.java",
     },
     ["src/test/java/*Test.java"] = {
       type = "test",
+      template = {
+        "package {dirname|dot};",
+        "",
+        "public class {basename}Test {open}",
+        "{close}",
+      },
       alternate = "src/main/java/{}.java",
     },
   },
