ebb4be1b2305a477f0b9c2e035711a38d3a155d2
Lars Martin <lars.martin.ext@zeiss.com>
2022-05-21 10:48:00 +0200

    use transparent background

--- dot_config/nvim/lua/user/colorscheme.lua
+++ dot_config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,7 @@
 vim.g.gruvbox_material_better_performance = 1
 vim.g.gruvbox_material_palette = "original"
 vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_transparent_background = 1
 vim.g.gruvbox_material_sign_column_background = "none"
 
 vim.cmd([[
