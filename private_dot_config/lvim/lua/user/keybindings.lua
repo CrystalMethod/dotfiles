7be25daff0df662fc09db376b8aeabfa818fb11d
Lars Martin <lars.martin@smb-tec.com>
2022-10-02 14:27:59 +0200

    add harpoon keymapping

--- private_dot_config/lvim/lua/user/keybindings.lua
+++ private_dot_config/lvim/lua/user/keybindings.lua
@@ -1,9 +1,37 @@
 local M = {}
 
+local function set_harpoon_keymaps()
+  lvim.keys.normal_mode["<C-Space>"] = "<cmd>lua require('harpoon.cmd-ui').toggle_quick_menu()<CR>"
+  lvim.keys.normal_mode["tu"] = "<cmd>lua require('harpoon.term').gotoTerminal(1)<CR>"
+  lvim.keys.normal_mode["te"] = "<cmd>lua require('harpoon.term').gotoTerminal(2)<CR>"
+  lvim.keys.normal_mode["cu"] = "<cmd>lua require('harpoon.term').sendCommand(1, 1)<CR>"
+  lvim.keys.normal_mode["ce"] = "<cmd>lua require('harpoon.term').sendCommand(1, 2)<CR>"
+  lvim.builtin.which_key.mappings["a"] = { "<cmd>lua require('harpoon.mark').add_file()<CR>", " Add Mark" }
+  lvim.builtin.which_key.mappings["<leader>"] = {
+    "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>",
+    " Harpoon",
+  }
+
+  local whk_status, whk = pcall(require, "which-key")
+  if not whk_status then
+    return
+  end
+  whk.register {
+    ["<leader>1"] = { "<CMD>lua require('harpoon.ui').nav_file(1)<CR>", " goto1" },
+    ["<leader>2"] = { "<CMD>lua require('harpoon.ui').nav_file(2)<CR>", " goto2" },
+    ["<leader>3"] = { "<CMD>lua require('harpoon.ui').nav_file(3)<CR>", " goto3" },
+    ["<leader>4"] = { "<CMD>lua require('harpoon.ui').nav_file(4)<CR>", " goto4" },
+  }
+end
+
 M.config = function()
   lvim.keys.insert_mode["<C-s>"] = "<cmd>lua vim.lsp.buf.signature_help()<cr>"
 
   lvim.keys.normal_mode["<esc><esc>"] = "<cmd>nohlsearch<cr>"
+
+  if lvim.builtin.harpoon.active then
+    set_harpoon_keymaps()
+  end
 
   if lvim.builtin.cheat.active then
     lvim.builtin.which_key.mappings["?"] = { "<cmd>Cheat<CR>", " Cheat.sh" }
