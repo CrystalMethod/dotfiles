d2c390d9db9b33060434dd4187b656b236ee47bf
Lars Martin <lars.martin@smb-tec.com>
2022-10-02 19:41:32 +0200

    customize gitsigns addon

--- private_dot_config/lvim/lua/user/builtin.lua
+++ private_dot_config/lvim/lua/user/builtin.lua
@@ -2,6 +2,11 @@ local M = {}
 
 M.config = function()
 
+  -- GitSigns
+  lvim.builtin.gitsigns.opts._threaded_diff = true
+  lvim.builtin.gitsigns.opts._extmark_signs = true
+  lvim.builtin.gitsigns.opts.current_line_blame_formatter = " <author>, <author_time> · <summary>"
+
 end
 
 return M
