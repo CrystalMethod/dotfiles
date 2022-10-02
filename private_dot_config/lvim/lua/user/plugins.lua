cbf69c262001b20224927db712e36a4fb62552d2
Lars Martin <lars.martin@smb-tec.com>
2022-10-02 10:22:26 +0200

    add lsp_signature addon

--- private_dot_config/lvim/lua/user/plugins.lua
+++ private_dot_config/lvim/lua/user/plugins.lua
@@ -6,6 +6,13 @@ M.config = function()
     -- Themes
     { "ellisonleao/gruvbox.nvim" },
     {
+      "ray-x/lsp_signature.nvim",
+      config = function()
+        require("user/lsp_signature").config()
+      end,
+      event = { "BufRead", "BufNew" },
+    },
+    {
       "folke/trouble.nvim",
       cmd = "TroubleToggle"
     },
