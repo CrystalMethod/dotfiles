d95c8555cd76fc4287e36c56efc7828ff474205b
Lars Martin <lars.martin@smb-tec.com>
2023-10-10 13:15:45 +0200

    add dependency axkirillov/easypick.nvim

--- home/private_dot_config/astronvim/lua/user/plugins/user.lua
+++ home/private_dot_config/astronvim/lua/user/plugins/user.lua
@@ -7,6 +7,31 @@ return {
     "CrystalMethod/codestats.nvim",
     event = "User AstroFile",
   },
+  {
+    "axkirillov/easypick.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Easypick",
+    config = function()
+      local base_branch = "develop"
+      local easypick = require("easypick")
+      easypick.setup({
+        pickers = {
+          {
+            name = "changed_files",
+            command = "git diff --name-only $(git merge-base HEAD " .. base_branch .. " )",
+            previewer = easypick.previewers.branch_diff({ base_branch = base_branch }),
+          },
+          {
+            name = "git_conflicts",
+            command = "git diff --name-only --diff-filter=U --relative",
+            previewer = easypick.previewers.file_diff(),
+          },
+        },
+      })
+    end,
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
