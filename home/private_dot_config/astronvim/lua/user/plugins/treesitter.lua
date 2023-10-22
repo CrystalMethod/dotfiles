c768d43157e269bbef20e69c18c337951a2e24d6
Lars Martin <lars.martin@smb-tec.com>
2023-10-22 21:21:44 +0200

    add nvim-treesitter/playground

--- home/private_dot_config/astronvim/lua/user/plugins/treesitter.lua
+++ home/private_dot_config/astronvim/lua/user/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/playground",
+  },
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
