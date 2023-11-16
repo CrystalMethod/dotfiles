e6a054e21e272c4b92bd07bf0f8fcfbb3844163c
Lars Martin <lars.martin@smb-tec.com>
2023-11-16 22:35:22 +0100

    better buffer navigation

--- home/private_dot_config/LazyVim/lua/config/keymaps.lua
+++ home/private_dot_config/LazyVim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- cursor stays in the middle of the screen when scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
