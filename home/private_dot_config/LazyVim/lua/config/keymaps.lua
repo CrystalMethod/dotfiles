86fd3e3fef19f8943174759785bb5a46fef01c91
Lars Martin <lars.martin@smb-tec.com>
2023-11-21 10:15:02 +0100

    add keymaps

--- home/private_dot_config/LazyVim/lua/config/keymaps.lua
+++ home/private_dot_config/LazyVim/lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
 -- cursor stays in the middle of the screen when scrolling
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
