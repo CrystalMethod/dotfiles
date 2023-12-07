480142c88bdcd0b9243ee0bb0b415e52ce3b89a7
Lars Martin <lars.martin@smb-tec.com>
2023-12-07 21:04:45 +0100

    add keymap for lazydocker + ctop

--- home/private_dot_config/LazyVim/lua/config/keymaps.lua
+++ home/private_dot_config/LazyVim/lua/config/keymaps.lua
@@ -2,9 +2,23 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local util = require("lazyvim.util")
+local wk = require("which-key")
+
+wk.register({
+  ["<leader>D"] = { name = "+docker" },
+})
+
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 -- cursor stays in the middle of the screen when scrolling
 keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<leader>Dl", function()
+  util.terminal.open({ "lazydocker" }, { cwd = util.root.get() })
+end, { desc = "Lazydocker" })
+
+keymap.set("n", "<leader>Dc", function()
+  util.terminal.open({ "ctop" }, { cwd = util.root.get() })
+end, { desc = "Ctop" })
